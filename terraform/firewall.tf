bddcc5632f730935e56a1e78c6859a563c4dd652
Michael Rolle <michael.rolle@bbd.co.za>
2024-05-29 14:31:46 +0200

    Added visibility config to WAF

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -25,9 +25,15 @@ resource "aws_wafv2_web_acl" "api_rate_limit_acl" {
     visibility_config {
       cloudwatch_metrics_enabled = false
       sampled_requests_enabled  = false
-      metric_name               = "RateLimitingMetric"
+      metric_name               = "RateLimitingMetricRule"
     }
   }
+
+  visibility_config {
+      cloudwatch_metrics_enabled = false
+      sampled_requests_enabled  = false
+      metric_name               = "RateLimitingMetric"
+    }
 }
 
 resource "aws_wafv2_web_acl_association" "api_rate_limit_acl_association" {
