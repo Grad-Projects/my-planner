e16abe57e1a7879e86727bc77091880ab23a268f
Ama <ama.commey@bbd.co.za>
2024-05-30 22:26:01 +0200

    main file

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const endpoints = require("./api/routes");
+const cors = require('cors');
 const app = express();
-const port = 5000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const port = 3000;
+
+app.use(cors())
+app.use(express.json());
+
+app.listen(port, () => {console.log(`Server has started on port: ${port} ✔️`)});
+
+app.use("/api/v1", endpoints);
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
