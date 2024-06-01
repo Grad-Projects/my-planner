9824adf18f6b5efe414f76abb30df7d295b22bc7
jennadunford <jenna.dunford@bbd.co.za>
2024-06-01 13:01:16 +0200

    added note card stuffs

--- frontend/scripts/weeklyListRender.js
+++ frontend/scripts/weeklyListRender.js
@@ -1,19 +1,30 @@
-const dayjs = require('dayjs');
 
 let weekViewList = document.getElementById("weekListView");
 
 let daysOfWeek = ["Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday","Sunday"];
-
+var now = dayjs();
 makeWeekList();
+
 
 function makeWeekList()
 {
-    daysOfWeek.forEach(day => {
-        const node = document.createElement("li");
-        const textnode = document.createTextNode(day);
-        node.appendChild(textnode);
-        weekViewList.appendChild(node);
-    });
-
+    const start = now.day();
+    let counter = 0;
+        for (let i = start-1; i < daysOfWeek.length + start-1; i++) 
+        {
+            const outerListNode = document.createElement("li");
+            const listNode = document.createElement("ul");
+            const innerListNodeDay = document.createElement("li");
+            const innerListNodeDate = document.createElement("li");
+            const textnodeDay = document.createTextNode(daysOfWeek[i % daysOfWeek.length]);
+            const textnodeDate = document.createTextNode(now.date() + counter);
+            innerListNodeDay.appendChild(textnodeDay);
+            innerListNodeDate.appendChild(textnodeDate);
+            listNode.appendChild(innerListNodeDay);
+            listNode.appendChild(innerListNodeDate);
+            outerListNode.appendChild(listNode);
+            weekViewList.appendChild(outerListNode);
+            counter++;
+        }
 }
 
