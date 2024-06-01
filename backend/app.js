ae6b4e841a4e2fa78ac012bbc6af3d06a6564933
Ama <ama.commey@bbd.co.za>
2024-06-01 12:08:17 +0200

    more security measures

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const endpoints = require("./api/routes");
+const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
+const fifteenMinutes = 15 * 60 * 1000;
+const apiLimiter = rateLimit({
+    windowMs: fifteenMinutes, // 15 minutes
+    max: 100, 
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+  });
+
 app.use(cors())
 app.use(express.json());
+app.use(helmet())
 
 app.listen(port, () => {console.log(`Server has started on port: ${port} ✔️`)});
 
-app.use("/api/v1", endpoints);
+app.use("/api/v1", apiLimiter, endpoints);
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
