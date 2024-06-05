28fcdb645d0216d40be9f1a852ca3cd2013661d2
Michael Rolle <michael.rolle@bbd.co.za>
2024-06-05 12:10:40 +0200

    Integrated login page to the single page application so it can load before everything else

--- frontend/scripts/authManager.js
+++ frontend/scripts/authManager.js
@@ -1,42 +1,31 @@
+import { backendUrl } from './apiConfig.js';
+
 function logout() {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("idToken");
     localStorage.removeItem("refreshToken");
     localStorage.removeItem("email");
-    window.location.href = "/pages/login.html";
+    window.location.href = "/index.html";
 }
 
-function base64UrlDecode(input) { 
-  let base64String = input.replace(/-/g, '+').replace(/_/g, '/');
-  let padding = 4 - (base64String.length % 4);
-  if (padding !== 4) {
-      for (let i = 0; i < padding; i++) {
-          base64String += '=';
-      }
-  }
-  const decodedData = atob(base64String);
-  const decodedString = decodeURIComponent(Array.from(decodedData).map((c) => {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-  return decodedString;
-}
-
-function decodeJWT(token = localStorage.getItem('jwtToken')) {
-  try {
-      const data = token.split('.')[1];
-      const stringify = (base64UrlDecode(data));
-      return JSON.parse(stringify);
-  } catch (error) {
-      console.error(error)
-      return null;
-  }
-}
-
-function isTokenValid() {
-    const token = localStorage.getItem("accessToken");
+async function isTokenValid(token) {
     if (!token) return false;
-    const payload = decodeJWT(token)
-    return !(!payload || Date.now() >= (payload.exp * 1000) - 30000);
+
+    try {
+        let response = await fetch(`${backendUrl}/api/v1/validate-token`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+        });
+
+        if (response.ok) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.error('Error verifying token');
+        return false;
+    }
 }
 
 export { logout, isTokenValid };
