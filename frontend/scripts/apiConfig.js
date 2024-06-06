1a3747bdbd3d2c02bddbec647bee17b625f46707
Michael Rolle <michael.rolle@bbd.co.za>
2024-06-06 14:53:14 +0200

    Logout now actually logs you out from cognito as well

--- frontend/scripts/apiConfig.js
+++ frontend/scripts/apiConfig.js
@@ -3,9 +3,11 @@ const clientId = '4k2e6jc066p8jsb6b86e90mm7j';
 const cognitoDomain = 'my-planner.auth.eu-west-1.amazoncognito.com';
 let backendUrl = 'https://myplannerapi.projects.bbdgrad.com';
 let redirectUri = 'https://myplanner.projects.bbdgrad.com/callback.html';
+let logoutRedirectUri = 'https://myplanner.projects.bbdgrad.com/logout.html';
 if (hostname.includes("localhost") || hostname.includes("127.0.0.1")) {
   backendUrl = 'https://localhost:8080';
   redirectUri = 'https://localhost:5500/callback.html';
+  logoutRedirectUri = 'https://localhost:5500/logout.html';
 }
 
-export { backendUrl, redirectUri, clientId, cognitoDomain };
+export { backendUrl, redirectUri, logoutRedirectUri, clientId, cognitoDomain };
